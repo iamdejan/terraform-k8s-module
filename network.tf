0a66ec0b1c273478095b8a004c9f81b23b3a2c34
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-07-22 23:05:35 +0700

    Remove secondary IP ranges

--- network.tf
+++ network.tf
@@ -10,14 +10,4 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = local.primary_cidr_range
-  secondary_ip_range = [
-    {
-      ip_cidr_range = local.pods_cidr_range
-      range_name    = "gke-${var.project_id}-gke-pods-72af974f"
-    },
-    {
-      ip_cidr_range = local.services_cidr_range
-      range_name    = "gke-${var.project_id}-gke-services-72af974f"
-    },
-  ]
 }
